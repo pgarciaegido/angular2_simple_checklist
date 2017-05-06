83372f2488c3fe924734c7e837432ccf86c5a717
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-05-06 18:53:54 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  // {name: 'Milk', done: false}
+  itemsAdded = [];
+  item = '';
+
+  addItem(item:string) {
+    if (item === '') return
+
+    let itemObject = {};
+    itemObject['name'] = item;
+    itemObject['done'] = false;
+    this.itemsAdded.push(itemObject);
+    console.log(this.itemsAdded);
+  }
+
+  // deleteItem(item:string) {
+  //   let index = this.itemsAdded.indexOf(item);
+  //   this.itemsAdded.splice(index, 1);
+  //   console.log(this.itemsAdded);
+  // }
 }
